7b6a66204f762c45892d8ad37a798fd473c872cb
behramsalar <135877779+behramsalar@users.noreply.github.com>
2025-10-21 23:17:37 +0000

    Deploying to gh-pages from @ behramsalar/cvdeneme2@0673b0613a7598609f154407b033656a1ed710c3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -7,35 +7,35 @@ ninja.data = [{
     title: "About",
     section: "Navigation",
     handler: () => {
-      window.location.href = "/cvdeneme/";
+      window.location.href = "/cvdeneme2/";
     },
   },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cvdeneme/blog/";
+            window.location.href = "/cvdeneme2/blog/";
           },
         },{id: "nav-publications",
           title: "Publications",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cvdeneme/publications/";
+            window.location.href = "/cvdeneme2/publications/";
           },
         },{id: "nav-misc",
           title: "Misc",
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cvdeneme/misc/";
+            window.location.href = "/cvdeneme2/misc/";
           },
         },{id: "nav-cv",
           title: "cv",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/cvdeneme/cv/";
+            window.location.href = "/cvdeneme2/cv/";
           },
         },{
       id: 'light-theme',
